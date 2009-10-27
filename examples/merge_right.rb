8c4adcd4ba0c4162b706d0c1fc435493ed42212b
aberant <qzzzq1@gmail.com>
2009-10-27 17:23:15 -0500

    first step in migration from spike. broke out header as class

--- examples/merge_right.rb
+++ examples/merge_right.rb
@@ -1,6 +1,6 @@
 require File.join( File.dirname( __FILE__ ), '..', 'lib', 'spittle' )
 
-png = PNG.open("pic_data/png_test.png")
-two = PNG.open("pic_data/png_test.png")
+png = PNG::Spike.open("pic_data/png_test.png")
+two = PNG::Spike.open("pic_data/png_test.png")
 out = png.merge_right(two)
 out.write("pic_data/out.png")
