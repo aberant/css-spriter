e05e50d739e6bb2557a684183ca19ec5dbaecdb3
Tyler Jennings <tjennings@kevin-taylors-powerbook-g4-12.local>
2009-10-30 14:37:04 -0500

    removed require of spike.rb from spittle.rb

--- lib/spittle.rb
+++ lib/spittle.rb
@@ -3,7 +3,6 @@ require 'zlib'
 $:.unshift( File.dirname( __FILE__ ))
 
 require 'spittle/file_header'
-require 'spittle/spike'
 require 'spittle/parser'
 require 'spittle/filters'
 require 'spittle/chunk'
