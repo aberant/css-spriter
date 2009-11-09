6f73312830b6e74d14765342200ed72652085ca7
aberant <qzzzq1@gmail.com>
2009-11-09 08:23:27 -0600

    putting merge left under sanity check

---
+++ spec/lib/image_spec.rb
@@ -0,0 +1,19 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe PNG::Image do
+  before :each do
+    @sprite = PNG::Sprite.new
+    
+    @builder = ImageBuilder.new
+    
+    # 0 is the filter type for the row, then an RGB triplet since builder defaults to color type 2
+    @image1 = @builder.build( :width => 1, :height => 1, :name => "image1", :data => [0,1,2,3] )
+    @image2 = @builder.build( :width => 1, :height => 1, :name => "image2", :data => [0,4,5,6] )
+  end
+
+  it "can merge left" do
+    result = @image1.merge_left @image2
+    
+    result.rows.should == [[4,5,6,1,2,3]]
+  end
+end
