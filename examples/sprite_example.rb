fa74477888269b59fdffc2ad46764c56b393fa15
aberant <qzzzq1@gmail.com>
2009-11-10 08:18:46 -0600

    benchmarking help

--- examples/sprite_example.rb
+++ examples/sprite_example.rb
@@ -1,11 +1,19 @@
 require File.join( File.dirname( __FILE__ ), '..', 'lib', 'spittle' )
 
-cat = PNG::Image.open( "pic_data/cat.png" )
-dog = PNG::Image.open( "pic_data/dog.png" )
-trash = PNG::Image.open( "pic_data/trash.png" )
+require 'benchmark'
+#
+result =  Benchmark.bm(10) do |x|
+  x.report {
+  cat = PNG::Image.open( "pic_data/cat.png" )
+  dog = PNG::Image.open( "pic_data/dog.png" )
+  trash = PNG::Image.open( "pic_data/trash.png" )
 
-sprite = PNG::Sprite.new
-sprite.merge_right cat
-sprite.merge_right dog
-sprite.merge_right trash
-sprite.write( "pic_data/animal.png")
+  sprite = PNG::Sprite.new
+  sprite.merge_right cat
+  sprite.merge_right dog
+  sprite.merge_right trash
+  sprite.write( "pic_data/animal.png")
+  }
+end
+
+puts result
