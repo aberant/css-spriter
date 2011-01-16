58c2bd48dddadcd978b50ec15e0fd894e57f6577
aberant <qzzzq1@gmail.com>
2011-01-16 14:16:44 -0600

    upgrading to rspec 2

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,11 @@
-# $LOAD_PATH.unshift(File.dirname(__FILE__))
-# $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-require 'spec'
-require 'spec/autorun'
+require 'rspec'
+require 'rspec/autorun'
 
 require 'css-spriter'
 
 require 'builders/image_builder'
 
-Spec::Runner.configure do |config|
+RSpec.configure do |config|
 
 end
 
