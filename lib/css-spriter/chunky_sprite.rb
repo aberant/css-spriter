8ee57700e643746342b626a593e84593cfdef8d8
aberant <qzzzq1@gmail.com>
2011-01-16 15:42:06 -0600

    hackey spikey first stab at replacing our png parser with chunky_png

---
+++ lib/css-spriter/chunky_sprite.rb
@@ -0,0 +1,36 @@
+require 'chunky_png'
+
+class ChunkySprite
+  attr_reader :images, :max_height
+
+  def initialize
+    @images = []
+    @locations = {}
+  end
+
+  def append( image )
+    @images << image
+  end
+
+  def append_file( filename )
+    append( ChunkyPNG::Image.from_file( filename ))
+  end
+
+  def write( output_filename )
+    return if @images.empty?
+
+    sprite_height = @images.map{ |i| i.height }.max
+    sprite_width = @images.inject(0){|sum, image| sum + image.width }
+
+    sprite = ChunkyPNG::Image.new(sprite_width, sprite_height)
+
+    current_x = 0
+
+    images.each do |image|
+      sprite.replace(image, current_x, 0)
+      current_x += image.width
+    end
+
+    sprite.save( output_filename, :best_compression )
+  end
+end
